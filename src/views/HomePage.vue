c43e83cdcd8fa0a8970f029e7a422780550c2a54
Viriyadhi <viriyadhitjan@gmail.com>
2021-11-05 15:55:48 +0700

    style:homepage

---
+++ src/views/HomePage.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Judul</h1>
+</template>
+
+<script>
+export default {
+  name: "HomePage",
+  mounted() {
+    this.$emit('changeRoute', this.$route.name);
+  }
+};
+</script>
+
+<style>
+
+</style>
